f6621a0538c09d757716812a09132c28130af747
HassanFdim <hassanfdim033@icloud.com>
2024-01-28 21:16:44 +0100

    first commit

---
+++ Todos.js
@@ -0,0 +1,6 @@
+let input = prompt("What do you like to do?");
+const todos = ['collect Ckecken Eggs' , 'clean litter Box '];
+while (input !== "quit" && input !== "q") {
+  let input = prompt("What do you like to do?");
+}
+console.log("OK QUIT THE APP");
